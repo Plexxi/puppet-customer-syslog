10b9b2a7fc183e90017074908a85e6cdd3a2b3cf
Matthew Morgan <matt.morgan@plexxi.com>
2018-05-10 13:39:44 -0400

    Version 1.0.3 fixed paramter validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -62,7 +62,7 @@
 #
 #  class { 'customer_syslog':
 #    enabled => true,
-#    entry_list => '[ { host => 192.168.1.1',
+#    entry_list => [ { host => "192.168.1.1",
 #                       port => 514,
 #                       facility => 'all',
 #                       severity => 'all'
@@ -78,59 +78,54 @@
 # Copyright 2018 Matthew Morgan, Plexxi, Inc
 #
 class customer_syslog (
-  $enabled,
-  $entry_list = [],
+  Boolean $enabled,
+  Array[Struct[{ host => String,
+                 port => Optional[Integer],
+                 facility => Pattern[/(?i:^all$)/,
+                                     /(?i:^kern$)/,
+                                      /(?i:^user$)/,
+                                      /(?i:^mail$)/,
+                                  /(?i:^daemon$)/,
+                                  /(?i:^auth$)/,
+                                  /(?i:^syslog$)/,
+                                  /(?i:^lpr$)/,
+                                  /(?i:^news$)/,
+                                  /(?i:^uucp$)/,
+                                  /(?i:^cron$)/,
+                                  /(?i:^security$)/,
+                                  /(?i:^ftp$)/,
+                                  /(?i:^ntp$)/,
+                                  /(?i:^logaudit$)/,
+                                  /(?i:^logalert$)/,
+                                  /(?i:^clock$)/,
+                                  /(?i:^local0$)/,
+                                  /(?i:^local1$)/,
+                                  /(?i:^local2$)/,
+                                  /(?i:^local3$)/,
+                                  /(?i:^local4$)/,
+                                  /(?i:^local5$)/,
+                                  /(?i:^local6$)/,
+                                  /(?i:^local7$)/],
+             severity => Pattern[/(?i:^all$)/,
+                                  /(?i:^emerg$)/,
+                                  /(?i:^alert$)/,
+                                  /(?i:^crit$)/,
+                                  /(?i:^error$)/,
+                                  /(?i:^warning$)/,
+                                  /(?i:^notice$)/,
+                                  /(?i:^info$)/,
+                                  /(?i:^debug$')/]             
+             }]] $entry_list = [],
 ) {
 
       #  $host       = undef,
       #  $port       = 514,
       #  $facility   = 'all',
       #  $severity   = 'all',
-      define validate_entries {
-          $approved_facility = [
-              '^all$',
-              '^kern$',
-              '^user$',
-              '^mail$',
-              '^daemon$',
-              '^auth$',
-              '^syslog$',
-              '^lpr$',
-              '^news$',
-              '^uucp$',
-              '^cron$',
-              '^security$',
-              '^ftp$',
-              '^ntp$',
-              '^logaudit$',
-              '^logalert$',
-              '^clock$',
-              '^local0$',
-              '^local1$',
-              '^local2$',
-              '^local3$',
-              '^local4$',
-              '^local5$',
-              '^local6$',
-              '^local7$'
-              ]
-          $approved_severity = [
-              '^all$',
-              '^emerg$',
-              '^alert$',
-              '^crit$',
-              '^error$',
-              '^warning$',
-              '^notice$',
-              '^info$',
-              '^debug$'
-              ]
-          validate_hash($name)
-          validate_re(downcase($name[facility]), $approved_facility)
-          validate_re(downcase($name[severity]), $approved_severity)
-          if has_key($name, port) {
-              if is_integer($name[port]) {
-                  if ($name[port] < 1) or ($name[port] > 65535) {
+      define validate_entries (Hash $entry = {}, ) {
+          if has_key($entry, port) {
+              if is_integer($entry[port]) {
+                  if ($entry[port] < 1) or ($entry[port] > 65535) {
                       fail('port must be an integer from 1 to 65535')
                   }
               }
@@ -138,21 +133,22 @@ class customer_syslog (
                   fail('port must be an integer')
               }
           }
-          if !has_key($name, host) {
+          if !has_key($entry, host) {
               fail('host must be provided for each list entry')
           }
       }
 
-      validate_bool($enabled)
-      validate_array($entry_list)
-      validate_entries { $entry_list: }
+      $entry_list.each |$value| {
+          customer_syslog::validate_entries { 'check_entry':
+                                              entry => $value }
+      }
       if $enabled { 
           file { '/etc/rsyslog.d/98customer.conf':
                   ensure  => file,
                   owner   => 0,
                   group   => 0,
                   mode    => '0644',
-                  content => template('customer-syslog/98customer.conf.erb'),
+                  content => template('customer_syslog/98customer.conf.erb'),
            }
            exec { 'rsyslog_restart':
                    command => '/usr/sbin/service rsyslog restart',
