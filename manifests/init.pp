81b9f2acd291c45decb7c809760839d633169a13
Joe Lorenz <joe.lorenz@plexxi.com>
2019-08-19 14:17:48 -0400

    PXSW-6300 - modify validation to allow multiple entries

--- manifests/init.pp
+++ manifests/init.pp
@@ -76,72 +76,57 @@
 # === Copyright
 #
 # Copyright 2018 Matthew Morgan, Plexxi, Inc
+# Copyright 2019 Joe Lorenz, HPE, Inc
 #
+
+type Facility = Pattern[/(?i:^all$)/,
+                        /(?i:^kern$)/,
+                        /(?i:^user$)/,
+                        /(?i:^mail$)/,
+                        /(?i:^daemon$)/,
+                        /(?i:^auth$)/,
+                        /(?i:^syslog$)/,
+                        /(?i:^lpr$)/,
+                        /(?i:^news$)/,
+                        /(?i:^uucp$)/,
+                        /(?i:^cron$)/,
+                        /(?i:^security$)/,
+                        /(?i:^ftp$)/,
+                        /(?i:^ntp$)/,
+                        /(?i:^logaudit$)/,
+                        /(?i:^logalert$)/,
+                        /(?i:^clock$)/,
+                        /(?i:^local0$)/,
+                        /(?i:^local1$)/,
+                        /(?i:^local2$)/,
+                        /(?i:^local3$)/,
+                        /(?i:^local4$)/,
+                        /(?i:^local5$)/,
+                        /(?i:^local6$)/,
+                        /(?i:^local7$)/]
+
+type Severity = Pattern[/(?i:^all$)/,
+                        /(?i:^emerg$)/,
+                        /(?i:^alert$)/,
+                        /(?i:^crit$)/,
+                        /(?i:^error$)/,
+                        /(?i:^warning$)/,
+                        /(?i:^notice$)/,
+                        /(?i:^info$)/,
+                        /(?i:^debug$')/]
+
+type Port = Integer[1, 65535]
+
+type SyslogEntry = Struct[{ host => String,
+                            port => Optional[Port],
+                            facility => Facility,
+                            severity => Severity
+                          }]
+
 class customer_syslog (
   Boolean $enabled,
-  Array[Struct[{ host => String,
-                 port => Optional[Integer],
-                 facility => Pattern[/(?i:^all$)/,
-                                     /(?i:^kern$)/,
-                                      /(?i:^user$)/,
-                                      /(?i:^mail$)/,
-                                  /(?i:^daemon$)/,
-                                  /(?i:^auth$)/,
-                                  /(?i:^syslog$)/,
-                                  /(?i:^lpr$)/,
-                                  /(?i:^news$)/,
-                                  /(?i:^uucp$)/,
-                                  /(?i:^cron$)/,
-                                  /(?i:^security$)/,
-                                  /(?i:^ftp$)/,
-                                  /(?i:^ntp$)/,
-                                  /(?i:^logaudit$)/,
-                                  /(?i:^logalert$)/,
-                                  /(?i:^clock$)/,
-                                  /(?i:^local0$)/,
-                                  /(?i:^local1$)/,
-                                  /(?i:^local2$)/,
-                                  /(?i:^local3$)/,
-                                  /(?i:^local4$)/,
-                                  /(?i:^local5$)/,
-                                  /(?i:^local6$)/,
-                                  /(?i:^local7$)/],
-             severity => Pattern[/(?i:^all$)/,
-                                  /(?i:^emerg$)/,
-                                  /(?i:^alert$)/,
-                                  /(?i:^crit$)/,
-                                  /(?i:^error$)/,
-                                  /(?i:^warning$)/,
-                                  /(?i:^notice$)/,
-                                  /(?i:^info$)/,
-                                  /(?i:^debug$')/]             
-             }]] $entry_list = [],
+  Array[SyslogEntry] $entry_list,
 ) {
-
-      #  $host       = undef,
-      #  $port       = 514,
-      #  $facility   = 'all',
-      #  $severity   = 'all',
-      define validate_entries (Hash $entry = {}, ) {
-          if has_key($entry, port) {
-              if is_integer($entry[port]) {
-                  if ($entry[port] < 1) or ($entry[port] > 65535) {
-                      fail('port must be an integer from 1 to 65535')
-                  }
-              }
-              else {
-                  fail('port must be an integer')
-              }
-          }
-          if !has_key($entry, host) {
-              fail('host must be provided for each list entry')
-          }
-      }
-
-      $entry_list.each |$value| {
-          customer_syslog::validate_entries { 'check_entry':
-                                              entry => $value }
-      }
       if $enabled { 
           file { '/etc/rsyslog.d/98customer.conf':
                   ensure  => file,
